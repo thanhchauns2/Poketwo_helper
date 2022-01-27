ee71fd59dbd569bf9add71bab93d3d1e82dd5cee
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-01-27 14:04:28 +0700

    Update shiny_remind_1.py

--- shiny_remind_1.py
+++ shiny_remind_1.py
@@ -1,7 +1,7 @@
 import discord
 import playsound
 
-ID = 934376539783573565
+ID = # channel ID
 
 client = discord.Client()
 
@@ -13,6 +13,6 @@ async def on_ready():
 async def on_message(message):
     channel = client.get_channel(ID)
     if message.content == "!gotem" and message.channel.id == ID:
-        playsound.playsound('C:/Users/Laptop88_LTV/Documents/GitHub/Python/Sound notification/hare hare ya.mp3')
+        playsound.playsound(sound directory)
 
-client.run("OTM1NzQwNzQyNDkwNTU0NDE5.YfDCqw.vOcTzMlCVtCMCWE5n7jvm5p8DF0", bot=False)
+client.run(token, bot=False)
